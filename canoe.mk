7ccdb99e8322c4cc068246079a594e48bd41cc97
V S Ganga VaraPrasad (VARA) Adabala <vadabala@qti.qualcomm.com>
2025-10-28 00:20:11 +0530

    compile whale manifest xml
    Change-Id: I7fe07257d10aa80380e684ea1071698ec87dffd4

--- canoe.mk
+++ canoe.mk
@@ -425,8 +425,9 @@ else
 TARGET_USES_QCV := true
 endif
 
-DEVICE_MANIFEST_SKUS := canoe alor
+DEVICE_MANIFEST_SKUS := canoe alor whale
 DEVICE_MANIFEST_CANOE_FILES := device/qcom/canoe/manifest_canoe.xml
+DEVICE_MANIFEST_WHALE_FILES := device/qcom/canoe/manifest_whale.xml
 DEVICE_MANIFEST_ALOR_FILES := device/qcom/canoe/manifest_alor.xml
 
 DEVICE_MATRIX_FILE   := device/qcom/common/compatibility_matrix.xml
